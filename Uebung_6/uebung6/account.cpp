f39ab1e6698925d7498e1fb9f69150bc82ac2ae9
Christopher Schulz <christopher.schulz@student.hpi.de>
2017-07-15 18:46:32 +0200

    implemented logged account inheritance

--- Uebung_6/uebung6/account.cpp
+++ Uebung_6/uebung6/account.cpp
@@ -14,11 +14,11 @@ public:
 
 	long accountNo() const { return accountNo_; }
 
-	void setLimit(double limit) { limit_ = limit; }
+	virtual void setLimit(double limit) { limit_ = limit; }
 	double getLimit() const { return limit_; }
 
-	bool credit(double amount);
-	bool debit(double amount);
+	virtual bool credit(double amount);
+	virtual bool debit(double amount);
 	double balance() const { return balance_; }
 
 	void setLock(bool locked) { locked_ = locked; }
@@ -31,6 +31,7 @@ private:
 	bool locked_;
 };
 
+//parent Class Methodes
 Account::Account(long accountNo, double balance, double limit, bool locked) :
 	accountNo_{accountNo},
 	balance_{balance},
@@ -66,11 +67,67 @@ bool Account::debit(double amount)
 	return true;
 }
 
+class LoggedAccount : public Account {
+public:
+	LoggedAccount(long accountNo, double balance, double limit, bool locked);
+
+	virtual void setLimit(double limit) override;
+	virtual bool credit(double amount) override;
+	virtual bool debit(double amount) override;
+
+	const list<pair<std::string, double>>& transactions();
+
+private:
+	list<pair<std::string, double>> transaction_list;
+};
+
+//inherited Class Methodes
+//ctor
+LoggedAccount::LoggedAccount(long accountNo, double balance, double limit, bool locked) :
+	Account(accountNo, balance, limit, locked),
+	transaction_list{ make_pair("**initial balance**",balance) }
+{}
+
+//transactions
+const list<pair<std::string, double>>& LoggedAccount::transactions()
+{
+	transaction_list.push_back(make_pair("**current balance**", balance()));
+	return transaction_list;
+}
+
+//Overrides
+void LoggedAccount::setLimit(double limit)
+{
+	Account::setLimit(limit);
+	transaction_list.push_back(make_pair("**set limit**", getLimit()));
+}
+
+bool LoggedAccount::credit(double amount)
+{
+	bool successful = Account::credit(amount);
+	if (successful){ 
+		transaction_list.push_back(make_pair("**credit**", amount));
+		return successful;
+	}
+	transaction_list.push_back(make_pair("**credit - failed - account locked**", amount));
+	return successful;
+}
+
+bool LoggedAccount::debit(double amount)
+{
+	bool successful = Account::debit(amount);
+	if (successful) {
+		transaction_list.push_back(make_pair("**debit**", amount));
+		return successful;
+	}
+	transaction_list.push_back(make_pair("**debit - failed - account locked or insufficent funds**", amount));
+	return successful;
+}
 
 int main(int argc, char** argv)
 {
 	Account A1(19920, 0.0, -1000.0, false);
-	/*LoggedAccount A2(20020, 0.0, -1000.0, false);
+	LoggedAccount A2(20020, 0.0, -1000.0, false);
 
 	A1.credit(500.0);
 	A2.credit(500.0);
@@ -80,7 +137,7 @@ int main(int argc, char** argv)
 	for (const auto& x : A2.transactions())
 	{
 		cout << x.first << ": " << x.second << endl;
-	}*/
+	}
 
 	return 0;
 }
