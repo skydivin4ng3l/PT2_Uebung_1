5fd756efba7ee75e948f853ab711da3b40299f09
Lasse Jahn <lasse.jahn@student.hpi.uni-potsdam.de>
2017-07-16 18:11:58 +0200

    add copy assignment

--- Uebung_6/uebung6/amount.cpp
+++ Uebung_6/uebung6/amount.cpp
@@ -25,10 +25,29 @@ class Amount
 
 	public:
 		Amount(): cur_{DEFAULT_CUR}, tr_{NORMAL}, netto_{0.0}, brutto_{0.0}, label_{"Nothing"} {}
-		Amount(std::string l, double n): cur_{DEFAULT_CUR}, tr_{NORMAL}, netto_{n}, brutto_{n*(static_cast<double>(tr_+100)/100)}, label_{l} {}
-		Amount(std::string l, double n, taxRate t): cur_{DEFAULT_CUR}, tr_{t}, netto_{n}, brutto_{n*(static_cast<double>(tr_+100)/100)}, label_{l} {}
-		Amount(std::string l, double n, currency c): cur_{c}, tr_{NORMAL}, netto_{n}, brutto_{n*(static_cast<double>(tr_+100)/100)}, label_{l} {}
-		Amount(std::string l, double n, currency c, taxRate t): cur_{c}, tr_{t}, netto_{n}, brutto_{n*(static_cast<double>(tr_+100)/100)}, label_{l} {}
+		Amount(std::string l, double n): cur_{DEFAULT_CUR}, tr_{NORMAL}, netto_{n}, brutto_{n*(static_cast<double>(tr_)+100.0)/100.0}, label_{l} {}
+		Amount(std::string l, double n, taxRate t): cur_{DEFAULT_CUR}, tr_{t}, netto_{n}, brutto_{n*(static_cast<double>(tr_)+100.0)/100.0}, label_{l} {}
+		Amount(std::string l, double n, currency c): cur_{c}, tr_{NORMAL}, netto_{n}, brutto_{n*(static_cast<double>(tr_)+100.0)/100.0}, label_{l} {}
+		Amount(std::string l, double n, currency c, taxRate t): cur_{c}, tr_{t}, netto_{n}, brutto_{n*(static_cast<double>(tr_)+100.0)/100.0}, label_{l} {}
+
+		//Copy assignment
+		Amount& operator=(const Amount& a){
+			cur_ = a.cur_;
+			tr_ = a.tr_;
+			this->setNetto(a.netto_);
+			label_ = a.label_;
+			return *this;
+		}
+
+		bool operator==(const Amount a){
+			if(cur_!=a.cur_) return false;
+			if(tr_!=a.tr_) return false;
+			if(netto_!=a.netto_) return false;
+			if(brutto_!=a.brutto_) return false;
+			if(label_.compare(a.label_) != 0) return false;
+
+			return true;
+		}
 
 		std::string getLabel(){
 			return label_;
@@ -47,7 +66,7 @@ class Amount
 		}
 
 		inline double getTaxFactor(){
-			return (static_cast<double>(tr_+100)/100);
+			return (static_cast<double>(tr_)+100.0)/100.0;
 		}
 
 		currency getCurrency(){
@@ -161,12 +180,20 @@ void test()
  	 		  && a2.getCurrency()==Amount::EUR && a2.getLabel().compare("Mango") == 0);
 
 	//Contructor
-	Amount a3 {"Mango", 1.0, Amount::EUR, Amount::REDUCED};
+	Amount a3 {"MangoUS", 0.8, Amount::USD, Amount::REDUCED};
 	std::cout << "Test constructor(label,netto,currency,taxrate)" << std::endl;
-	std::cout << "Example constructor(\"Mango\",1.0,EUR,REDUCED)" << std::endl;
+	std::cout << "Example constructor(\"MangoUS\",0.8,USD,REDUCED)" << std::endl;
 	std::cout << a3 << std::endl;
-	assert(a3.getNetto()==1.0 && a3.getBrutto() == 1.0*1.07 && a3.getTaxRate()==Amount::REDUCED
-	 			 && a3.getCurrency()==Amount::EUR && a3.getLabel().compare("Mango") == 0);
+	assert(a3.getNetto()==0.8 && a3.getBrutto() == 0.8*1.07 && a3.getTaxRate()==Amount::REDUCED
+	 			 && a3.getCurrency()==Amount::USD && a3.getLabel().compare("MangoUS") == 0);
+
+
+	//Copy assignment
+	a3 = a2;
+	std::cout << "Set MangoUS to Mango, with copy assignment" << std::endl;
+	std::cout << a3 << std::endl;
+	assert(a3==a2);
+
 
 	 //setCurrency()
 	std::cout << "Change Currency from EUR to USD" << std::endl;
